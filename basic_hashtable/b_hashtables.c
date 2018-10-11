aad86ea61c21a33a1befd00f92e2880815f44b0b
sean chavez <seanchavez@seanchavezsMBP.home>
2018-10-11 09:20:39 -0700

    Implement *create_hash_table

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -67,6 +67,8 @@ unsigned int hash(char *str, int max)
 BasicHashTable *create_hash_table(int capacity)
 {
   BasicHashTable *ht = malloc(sizeof(BasicHashTable));
+  ht->capacity = capacity;
+  ht->storage = calloc(capacity, sizeof(Pair *));
 
   return ht;
 }
