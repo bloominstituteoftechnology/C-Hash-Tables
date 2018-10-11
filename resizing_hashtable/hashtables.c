a5eae2f460b58aaa4cad2c08e8bc45273f03f2d3
Tyson Gushiken <tysonism@gmail.com>
2018-10-11 15:56:18 -0700

    Fill in hash table resize

--- resizing_hashtable/hashtables.c
+++ resizing_hashtable/hashtables.c
@@ -153,7 +153,15 @@ void destroy_hash_table(HashTable *ht)
  ****/
 HashTable *hash_table_resize(HashTable *ht)
 {
-  HashTable *new_ht;
+  HashTable *new_ht = malloc(sizeof(HashTable));
+  new_ht->capacity = ht->capacity * 2;
+  new_ht->storage = calloc(new_ht->capacity, sizeof(LinkedPair*));
+
+  for(int i = 0; i < ht->capacity; i++) {
+    new_ht->storage[i] = ht->storage[i];
+    ht->storage[i] = NULL;
+  }
+  destroy_hash_table(ht);
 
   return new_ht;
 }
