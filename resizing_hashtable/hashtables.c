88fe8a24f49e4788637f99611b19203bae4452c6
emaquino44 <prexy44@yahoo.com>
2018-10-11 21:17:54 -0700

    Finished hashtables.c - added solution

--- resizing_hashtable/hashtables.c
+++ resizing_hashtable/hashtables.c
@@ -1,49 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
 /****
-  Hash table key/value pair with linked list pointer
+  Basic hash table key/value pair
  ****/
 typedef struct LinkedPair {
   char *key;
   char *value;
   struct LinkedPair *next;
 } LinkedPair;
-
 /****
-  Hash table with linked pairs
+  Basic hash table
  ****/
-typedef struct HashTable {
+typedef struct BasicHashTable {
   int capacity;
   LinkedPair **storage;
-} HashTable;
-
+} BasicHashTable;
 /****
-  Create a key/value linked pair to be stored in the hash table.
+  Create a key/value pair to be stored in the hash table.
  ****/
 LinkedPair *create_pair(char *key, char *value)
 {
   LinkedPair *pair = malloc(sizeof(LinkedPair));
-  pair->key = key;
-  pair->value = value;
+  pair->key = strdup(key);
+  pair->value = strdup(value);
   pair->next = NULL;
-
   return pair;
 }
-
 /****
   Use this function to safely destroy a hashtable pair.
  ****/
 void destroy_pair(LinkedPair *pair)
 {
-  if (pair != NULL) free(pair);
+  if (pair != NULL) {
+    free(pair->key);
+    free(pair->value);
+    free(pair);
+  }
 }
-
 /****
   djb2 hash function
-
   Do not modify this!
  ****/
 unsigned int hash(char *str, int max)
@@ -51,113 +47,105 @@ unsigned int hash(char *str, int max)
   unsigned long hash = 5381;
   int c;
   unsigned char * u_str = (unsigned char *)str;
-
   while ((c = *u_str++)) {
     hash = ((hash << 5) + hash) + c;
   }
-
+  hash = 1;
   return hash % max;
 }
-
 /****
   Fill this in.
-
   All values in storage should be initialized to NULL
+  (hint: look up `calloc`)
  ****/
-HashTable *create_hash_table(int capacity)
+BasicHashTable *create_hash_table(int capacity)
 {
-  HashTable *ht;
+  BasicHashTable *ht;
+    ht = malloc(sizeof(BasicHashTable));
+
+    ht->capacity = capacity;
+    ht->storage = calloc(capacity, sizeof(LinkedPair *));
 
   return ht;
 }
-
 /****
   Fill this in.
-
-  Inserting values to the same index with different keys should be
-  added to the corresponding LinkedPair list.
-
-  Inserting values to the same index with existing keys can overwrite
-  the value in th existing LinkedPair list.
- ****/
-void hash_table_insert(HashTable *ht, char *key, char *value)
-{
-
-}
-
-/****
-  Fill this in.
-
-  Should search the entire list of LinkedPairs for existing
-  keys and remove matching LinkedPairs safely.
-
+  If you are overwriting a value with a different key, print a warning.
   Don't forget to free any malloc'ed memory!
  ****/
-void hash_table_remove(HashTable *ht, char *key)
+void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
-
+  unsigned int index = hash(key, ht->capacity);
+  LinkedPair *current_pair = ht->storage[index];
+  LinkedPair *last_pair;
+  while (current_pair != NULL && strcmp(current_pair->key, key) != 0) {
+    last_pair = current_pair;
+    current_pair = last_pair->next;
+  }
+  if (current_pair != NULL) {
+    current_pair->value = value;
+  } else {
+    LinkedPair *new_pair = create_pair(key, value);
+    new_pair->next = ht->storage[index];
+    ht->storage[index] = new_pair;
+  }
 }
-
 /****
   Fill this in.
-
-  Should search the entire list of LinkedPairs for existing
-  keys.
-
-  Return NULL if the key is not found.
- ****/
-char *hash_table_retrieve(HashTable *ht, char *key)
-{
-  return NULL;
-}
-
-/****
-  Fill this in.
-
   Don't forget to free any malloc'ed memory!
  ****/
-void destroy_hash_table(HashTable *ht)
+void hash_table_remove(BasicHashTable *ht, char *key)
 {
-
+  unsigned int index = hash(key, ht->capacity);
+  if (ht->storage[index] == NULL) {
+    printf("Unable to remove entry with key: %s\n", key);
+  } else {
+    destroy_pair(ht->storage[index]);
+    ht->storage[index] = NULL;
+  }
 }
-
 /****
   Fill this in.
-
-  Should create a new hash table with double the capacity
-  of the original and copy all elements into the new hash table.
-
+  Should return NULL if the key is not found.
+ ****/
+char *hash_table_retrieve(BasicHashTable *ht, char *key)
+{
+  unsigned int index = hash(key, ht->capacity);
+  if (ht->storage[index] == NULL) {
+    printf("Unable to retrieve entry with key: %s", key);
+    return NULL;
+  }
+  return ht->storage[index]->value;
+}
+/****
+  Fill this in.
   Don't forget to free any malloc'ed memory!
  ****/
-HashTable *hash_table_resize(HashTable *ht)
+void destroy_hash_table(BasicHashTable *ht)
 {
-  HashTable *new_ht;
-
-  return new_ht;
+  for (int i = 0 ; i < ht->capacity ; i++) {
+    if (ht->storage[i] != NULL) {
+      destroy_pair(ht->storage[i]);
+    }
+  }
+  free(ht->storage);
+  free(ht);
 }
-
-
 #ifndef TESTING
 int main(void)
 {
-  struct HashTable *ht = create_hash_table(2);
-
-  hash_table_insert(ht, "line_1", "Tiny hash table\n");
-  hash_table_insert(ht, "line_2", "Filled beyond capacity\n");
-  hash_table_insert(ht, "line_3", "Linked list saves the day!\n");
-
-  printf("%s", hash_table_retrieve(ht, "line_1"));
-  printf("%s", hash_table_retrieve(ht, "line_2"));
-  printf("%s", hash_table_retrieve(ht, "line_3"));
-
-  int old_capacity = ht->capacity;
-  ht = hash_table_resize(ht);
-  int new_capacity = ht->capacity;
-
-  printf("\nResizing hash table from %d to %d.\n", old_capacity, new_capacity);
-
+  struct BasicHashTable *ht = create_hash_table(2);
+  hash_table_insert(ht, "line", "Here today...\n");
+  hash_table_insert(ht, "newline", "newkey\n");
+  hash_table_insert(ht, "anotherline", "anotherkey\n");
+  printf("%s", hash_table_retrieve(ht, "line"));
+  hash_table_remove(ht, "line");
+  if (hash_table_retrieve(ht, "line") == NULL) {
+    printf("...gone tomorrow. (success)\n");
+  } else {
+    fprintf(stderr, "ERROR: STILL HERE\n");
+  }
   destroy_hash_table(ht);
-
   return 0;
 }
 #endif
