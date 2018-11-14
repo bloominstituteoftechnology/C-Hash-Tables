e03202cf745f4209746aace8b2da584332eed9d2
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-11-14 15:11:38 -0600

    finished create hash table

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -67,10 +67,8 @@ unsigned int hash(char *str, int max)
 BasicHashTable *create_hash_table(int capacity)
 {
   BasicHashTable *ht;
-  int n;
-  
-  ht = calloc(n,sizeof(capacity));
-
+  ht->capacity = capacity;
+  ht->storage = calloc(capacity, sizeof(Pair *));
   return ht;
 }
 
