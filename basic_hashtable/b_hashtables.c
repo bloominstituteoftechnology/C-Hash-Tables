45621e26b86ec6d4512747e2ec207b95505f5e04
codejoncode <jonathanjholloway@gmail.com>
2018-11-14 15:13:45 -0600

    Organized the file to have prototypes structures areas and then started the functions, I believe I have completed create_hash_table and hash_table_insert. I used calloc on the storage so that all values are null. I also print out two error messages one for if the pair being set to a previously set hash index and another if the key doesn't match the previously set key.

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//https://repl.it/@codejoncode/Hash-Table-Assignment
 
-
+/*Structures begin*/
 /****
   Basic hash table key/value pair
  ****/
@@ -18,7 +19,47 @@ typedef struct BasicHashTable {
   int capacity;
   Pair **storage;
 } BasicHashTable;
+/*Structures end*/
 
+
+/*Function prototypes begin*/
+Pair *create_pair(char *key, char *value);
+void destroy_pair(Pair *pair);
+unsigned int hash(char *str, int max);
+BasicHashTable *create_hash_table(int capacity);
+void hash_table_insert(BasicHashTable *ht, char *key, char *value);
+void hash_table_remove(BasicHashTable *ht, char *key);
+char *hash_table_retrieve(BasicHashTable *ht, char *key);
+void destroy_hash_table(BasicHashTable *ht);
+/*Function prototypes end*/
+
+
+
+#ifndef TESTING
+int main(void)
+{
+  struct BasicHashTable *ht = create_hash_table(16);
+
+  hash_table_insert(ht, "line", "Here today...\n");
+
+  printf("%s", hash_table_retrieve(ht, "line"));
+
+  hash_table_remove(ht, "line");
+
+  if (hash_table_retrieve(ht, "line") == NULL) {
+    printf("...gone tomorrow. (success)\n");
+  } else {
+    fprintf(stderr, "ERROR: STILL HERE\n");
+  }
+
+  destroy_hash_table(ht);
+
+  return 0;
+}
+#endif
+
+
+/*Functions begin*/
 /****
   Create a key/value pair to be stored in the hash table.
  ****/
@@ -30,7 +71,6 @@ Pair *create_pair(char *key, char *value)
 
   return pair;
 }
-
 /****
   Use this function to safely destroy a hashtable pair.
  ****/
@@ -66,7 +106,11 @@ unsigned int hash(char *str, int max)
  ****/
 BasicHashTable *create_hash_table(int capacity)
 {
-  BasicHashTable *ht;
+  BasicHashTable *ht = malloc(sizeof(BasicHashTable));
+  ht->capacity = capacity; 
+
+  ht->storage = calloc(ht->capacity, sizeof(Pair*));
+  //^this is the line that sets the storage to null spaces. 
 
   return ht;
 }
@@ -80,7 +124,23 @@ BasicHashTable *create_hash_table(int capacity)
  ****/
 void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
+  Pair *pair = create_pair(key, value); 
+  //^ create the pair to add to the hash table 
 
+  //Get a hash 
+  int hash_index = hash(key, ht->capacity);
+  //Check if the space is taken up already will be NULL due to the use of calloc when creating the hash table storage 
+  if (ht->storage[hash_index] != NULL){
+    printf("\nERROR potential of losing previous omitted data! Overwriting\n");
+    if(strcmp(ht->storage[hash_index]->key, key) != 0  ){
+      printf("\nERROR-The key you previously set doesn't match the new key being set. Overwriting \n");
+    }
+  }
+
+
+    
+  //Overwrite/ set hash to equal the pair. 
+  ht->storage[hash_index] = pair; 
 }
 
 /****
@@ -112,27 +172,3 @@ void destroy_hash_table(BasicHashTable *ht)
 {
 
 }
-
-
-#ifndef TESTING
-int main(void)
-{
-  struct BasicHashTable *ht = create_hash_table(16);
-
-  hash_table_insert(ht, "line", "Here today...\n");
-
-  printf("%s", hash_table_retrieve(ht, "line"));
-
-  hash_table_remove(ht, "line");
-
-  if (hash_table_retrieve(ht, "line") == NULL) {
-    printf("...gone tomorrow. (success)\n");
-  } else {
-    fprintf(stderr, "ERROR: STILL HERE\n");
-  }
-
-  destroy_hash_table(ht);
-
-  return 0;
-}
-#endif
