fb25d6dbd4a498b8ca2fffce4ef1dbfe91e8e948
rverdi642 <rverdi642@gmail.com>
2018-11-14 17:22:28 -0600

    Richard Verdier - passed tests

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -67,6 +67,9 @@ unsigned int hash(char *str, int max)
 BasicHashTable *create_hash_table(int capacity)
 {
   BasicHashTable *ht;
+  ht = malloc(sizeof(BasicHashTable));
+  ht->capacity = capacity;
+  ht->storage = calloc(capacity, sizeof(Pair));
 
   return ht;
 }
@@ -81,6 +84,16 @@ BasicHashTable *create_hash_table(int capacity)
 void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
 
+unsigned int index = hash(key, ht->capacity);
+  Pair *current_pair = ht->storage[index];
+
+  if (current_pair != NULL) {
+    fprintf(stderr, "Warning: Value %s at Key %s will be overwritten!\n", 
+        current_pair->value, key);
+  }
+
+  ht->storage[index] = create_pair(key, value);
+
 }
 
 /****
@@ -91,6 +104,17 @@ void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 void hash_table_remove(BasicHashTable *ht, char *key)
 {
 
+unsigned int index = hash(key, ht->capacity);
+  Pair *current_pair = ht->storage[index];
+
+  if (current_pair && strcmp(current_pair->key, key) == 0) {
+    destroy_pair(current_pair);
+    ht->storage[index] = NULL;
+  }
+  else {
+    fprintf(stderr, "Error: Key not found.\n");
+  }
+
 }
 
 /****
@@ -100,7 +124,15 @@ void hash_table_remove(BasicHashTable *ht, char *key)
  ****/
 char *hash_table_retrieve(BasicHashTable *ht, char *key)
 {
-  return NULL;
+  unsigned int index = hash(key, ht->capacity);
+  Pair *current_pair = ht->storage[index];
+
+  if (current_pair && strcmp(current_pair->key, key) == 0) {
+    return current_pair->value;
+  }
+  else {
+    return NULL;
+  }
 }
 
 /****
@@ -110,6 +142,16 @@ char *hash_table_retrieve(BasicHashTable *ht, char *key)
  ****/
 void destroy_hash_table(BasicHashTable *ht)
 {
+
+for (int i = 0; i < ht->capacity; i++) {
+    destroy_pair(ht->storage[i]);
+  }
+  if (ht->storage != NULL) {
+    free(ht->storage);
+  }
+  if (ht != NULL) {
+    free(ht);
+  }
 
 }
 
