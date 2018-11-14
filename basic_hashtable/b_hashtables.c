dc124825f07c954a45aadafd29b88a5561b1d862
William <kwontaewoongah@gmail.com>
2018-11-14 14:38:29 -0800

    freeing individual pairs in destroy hash table

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -138,6 +138,12 @@ char *hash_table_retrieve(BasicHashTable *ht, char *key)
  ****/
 void destroy_hash_table(BasicHashTable *ht)
 {
+  for (int i=0; i<ht->capacity; i++) {
+    if (ht->storage[i]!=NULL) {
+      destroy_pair(ht->storage[i]);
+      ht->storage[i]=NULL;
+    }
+  }
   free(ht);
 }
 
