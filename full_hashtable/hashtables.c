3c00386c688d6847def6ba85eaa81069c3788a2c
codejoncode <jonathanjholloway@gmail.com>
2018-11-15 14:32:37 -0600

    create_hash_table completed I believe

--- full_hashtable/hashtables.c
+++ full_hashtable/hashtables.c
@@ -106,7 +106,10 @@ unsigned int hash(char *str, int max)
  ****/
 HashTable *create_hash_table(int capacity)
 {
-  HashTable *ht;
+  HashTable *ht = malloc(sizeof(HashTable)); 
+  ht->capacity = capacity; 
+
+  ht->storage = calloc(ht->capacity, sizeof(LinkedPair*));
 
   return ht;
 }
