264006462bd70bd192f6e5f788ae9b6f0f48b0e1
Munsu <dark_specineff2002@yahoo.com>
2018-11-15 15:48:53 -0800

    hash_table_insert, destroy_hash_table, hash_table_resize

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -125,6 +125,10 @@ char *hash_table_retrieve(BasicHashTable *ht, char *key)
  ****/
 void destroy_hash_table(BasicHashTable *ht)
 {
+  for (int i = 0; i < ht->capacity; i++) {
+    destroy_pair(ht->storage[i]);
+  }
+  free(ht->storage);
   free(ht);
 }
 
