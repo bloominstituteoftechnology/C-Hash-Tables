5cadecc456005a082dfc0146333b836d893434a2
Joseph Chretien-Golden <joechre@gmail.com>
2019-01-16 17:56:29 -0600

    INSERT -establish index use hash function - storedpair declared -null out pair in hash table CREATE -mem alloc -capacity set -calloc implementation

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -70,8 +70,11 @@ unsigned int hash(char *str, int max)
  ****/
 BasicHashTable *create_hash_table(int capacity)
 {
-  BasicHashTable *ht;
+   BasicHashTable *ht = malloc(sizeof(BasicHashTable));//memory allocation
+  
 
+  ht->capacity = capacity; //setting capacity (interger)
+  ht->storage = calloc(capacity, sizeof(Pair*));
   return ht;
 }
 
@@ -84,9 +87,17 @@ BasicHashTable *create_hash_table(int capacity)
  ****/
 void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
-
+  //est index, convert string to array with hash function
+  unsigned int index = hash(key, ht->capacity);
+  Pair* storedpair = ht->storage[index];//declared stored pair in storage w/index
+  if (ht->storage != NULL) {
+    if (strcmp(key, storedpair->key) != 0){
+      printf ("this a warning!");
+    }
+    destroy_pair(storedpair);
+  }
+  ht->storage[index] = NULL; // null out the pair that lives in the hash table
 }
-
 /****
   Fill this in.
 
