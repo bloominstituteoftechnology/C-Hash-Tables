196098f9f31dec7a377a4d95aa224f27153cbff8
iAmAdamReid <adam@adamreid.me>
2019-01-16 15:14:46 -0800

    Basic hashtable passes all tests and compiles with no errors.

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -70,7 +70,9 @@ unsigned int hash(char *str, int max)
  ****/
 BasicHashTable *create_hash_table(int capacity)
 {
-  BasicHashTable *ht;
+  BasicHashTable *ht = malloc(sizeof(BasicHashTable));
+  ht->capacity = capacity;
+  ht->storage = calloc(capacity, sizeof(Pair *));
 
   return ht;
 }
@@ -84,7 +86,15 @@ BasicHashTable *create_hash_table(int capacity)
  ****/
 void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
+  int index = hash(key, ht->capacity);
 
+  if(ht->storage[index] != NULL){
+    printf("WARNING: This will overwrite an existing value.\n");
+    destroy_pair(ht->storage[index]);
+    ht->storage[index] = create_pair(key, value);
+  } else {
+    ht->storage[index] = create_pair(key, value);
+  }
 }
 
 /****
@@ -94,6 +104,10 @@ void hash_table_insert(BasicHashTable *ht, char *key, char *value)
  ****/
 void hash_table_remove(BasicHashTable *ht, char *key)
 {
+  int index = hash(key, ht->capacity);
+
+  destroy_pair(ht->storage[index]); // deep deletion of pair in hashtable
+  ht->storage[index] = NULL; // reset ht index value to NULL
 
 }
 
@@ -104,7 +118,14 @@ void hash_table_remove(BasicHashTable *ht, char *key)
  ****/
 char *hash_table_retrieve(BasicHashTable *ht, char *key)
 {
+  int index = hash(key, ht->capacity);
+
+  if(ht->storage[index] != NULL){
+    return ht->storage[index]->value;
+  }
+
   return NULL;
+
 }
 
 /****
@@ -114,6 +135,12 @@ char *hash_table_retrieve(BasicHashTable *ht, char *key)
  ****/
 void destroy_hash_table(BasicHashTable *ht)
 {
+  for(int i = 0; i < ht->capacity; i++){
+    destroy_pair(ht->storage[i]); // iterate over storage and free each pair
+  }
+
+  free(ht->storage); // free storage
+  free(ht); // finally, free the hash table
 
 }
 
