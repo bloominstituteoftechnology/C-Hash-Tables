f6369979dd528876fce3ba8f2aefc2f89b189f59
Nicocchi <nicocchilover@gmail.com>
2019-01-16 16:03:48 -0500

    destroy hashtable complete

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -146,7 +146,12 @@ char *hash_table_retrieve(BasicHashTable *ht, char *key)
  ****/
 void destroy_hash_table(BasicHashTable *ht)
 {
+  for (int i = 0; i < ht->capacity; i++) {
+    destroy_pair(ht->storage[i]);
+  }
 
+  free(ht->storage);
+  free(ht);
 }
 
 
