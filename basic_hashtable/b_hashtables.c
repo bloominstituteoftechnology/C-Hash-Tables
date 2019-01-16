261e2dad9bf5675704f02f71ad22dc505bfaa6ce
Anthony Greb <agreb255@gmail.com>
2019-01-16 18:27:02 -0500

    working on destroy,set up if to avoid double free

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -126,6 +126,13 @@ char *hash_table_retrieve(BasicHashTable *ht, char *key)
  ****/
 void destroy_hash_table(BasicHashTable *ht)
 {
+  for ( int i = 0; i < ht->capacity; i++ ) {
+    destroy_pair(ht->storage[i]);
+    if ( ht->storage[i] != NULL ) {
+        free(ht->storage);
+    }
+  }
+  free(ht);
 
 }
 
