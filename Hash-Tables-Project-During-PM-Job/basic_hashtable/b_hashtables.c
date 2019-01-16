c73bd450bb2b77bace20f79f6dcbe0e301d9d92c
Adrian Adames <adrianadames@gmail.com>
2019-01-16 15:26:40 -0500

    Added folder containing the newly updated hash tables project (i.e. the hash tables project assigned to students during my time as a project manager.

---
+++ Hash-Tables-Project-During-PM-Job/basic_hashtable/b_hashtables.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/****
+  Basic hash table key/value pair
+ ****/
+typedef struct Pair {
+  char *key;
+  char *value;
+} Pair;
+
+/****
+  Basic hash table
+ ****/
+typedef struct BasicHashTable {
+  int capacity;
+  Pair **storage;
+} BasicHashTable;
+
+/****
+  Create a key/value pair to be stored in the hash table.
+ ****/
+Pair *create_pair(char *key, char *value)
+{
+  Pair *pair = malloc(sizeof(Pair));
+  pair->key = strdup(key);
+  pair->value = strdup(value);
+
+  return pair;
+}
+
+/****
+  Use this function to safely destroy a hashtable pair.
+ ****/
+void destroy_pair(Pair *pair)
+{
+  if (pair != NULL) {
+    free(pair->key);
+    free(pair->value);
+    free(pair);
+  }
+}
+
+/****
+  djb2 hash function
+
+  Do not modify this!
+ ****/
+unsigned int hash(char *str, int max)
+{
+  unsigned long hash = 5381;
+  int c;
+  unsigned char * u_str = (unsigned char *)str;
+
+  while ((c = *u_str++)) {
+    hash = ((hash << 5) + hash) + c;
+  }
+
+  return hash % max;
+}
+
+
+/****
+  Fill this in.
+
+  All values in storage should be initialized to NULL
+  (hint: look up `calloc`)
+ ****/
+BasicHashTable *create_hash_table(int capacity)
+{
+  BasicHashTable *ht;
+
+  return ht;
+}
+
+/****
+  Fill this in.
+
+  If you are overwriting a value with a different key, print a warning.
+
+  Don't forget to free any malloc'ed memory!
+ ****/
+void hash_table_insert(BasicHashTable *ht, char *key, char *value)
+{
+
+}
+
+/****
+  Fill this in.
+
+  Don't forget to free any malloc'ed memory!
+ ****/
+void hash_table_remove(BasicHashTable *ht, char *key)
+{
+
+}
+
+/****
+  Fill this in.
+
+  Should return NULL if the key is not found.
+ ****/
+char *hash_table_retrieve(BasicHashTable *ht, char *key)
+{
+  return NULL;
+}
+
+/****
+  Fill this in.
+
+  Don't forget to free any malloc'ed memory!
+ ****/
+void destroy_hash_table(BasicHashTable *ht)
+{
+
+}
+
+
+#ifndef TESTING
+int main(void)
+{
+  struct BasicHashTable *ht = create_hash_table(16);
+
+  hash_table_insert(ht, "line", "Here today...\n");
+
+  printf("%s", hash_table_retrieve(ht, "line"));
+
+  hash_table_remove(ht, "line");
+
+  if (hash_table_retrieve(ht, "line") == NULL) {
+    printf("...gone tomorrow. (success)\n");
+  } else {
+    fprintf(stderr, "ERROR: STILL HERE\n");
+  }
+
+  destroy_hash_table(ht);
+
+  return 0;
+}
+#endif
---
+++ Hash-Tables-Project-During-PM-Job/full_hashtable/hashtables.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/****
+  Hash table key/value pair with linked list pointer
+ ****/
+typedef struct LinkedPair {
+  char *key;
+  char *value;
+  struct LinkedPair *next;
+} LinkedPair;
+
+/****
+  Hash table with linked pairs
+ ****/
+typedef struct HashTable {
+  int capacity;
+  LinkedPair **storage;
+} HashTable;
+
+/****
+  Create a key/value linked pair to be stored in the hash table.
+ ****/
+LinkedPair *create_pair(char *key, char *value)
+{
+  LinkedPair *pair = malloc(sizeof(LinkedPair));
+  pair->key = strdup(key);
+  pair->value = strdup(value);
+  pair->next = NULL;
+
+  return pair;
+}
+
+/****
+  Use this function to safely destroy a hashtable pair.
+ ****/
+void destroy_pair(LinkedPair *pair)
+{
+  if (pair != NULL) {
+    free(pair->key);
+    free(pair->value);
+    free(pair);
+  }
+}
+
+/****
+  djb2 hash function
+
+  Do not modify this!
+ ****/
+unsigned int hash(char *str, int max)
+{
+  unsigned long hash = 5381;
+  int c;
+  unsigned char * u_str = (unsigned char *)str;
+
+  while ((c = *u_str++)) {
+    hash = ((hash << 5) + hash) + c;
+  }
+
+  return hash % max;
+}
+
+/****
+  Fill this in.
+
+  All values in storage should be initialized to NULL
+ ****/
+HashTable *create_hash_table(int capacity)
+{
+  HashTable *ht;
+
+  return ht;
+}
+
+/****
+  Fill this in.
+
+  Inserting values to the same index with different keys should be
+  added to the corresponding LinkedPair list.
+
+  Inserting values to the same index with existing keys can overwrite
+  the value in th existing LinkedPair list.
+ ****/
+void hash_table_insert(HashTable *ht, char *key, char *value)
+{
+
+}
+
+/****
+  Fill this in.
+
+  Should search the entire list of LinkedPairs for existing
+  keys and remove matching LinkedPairs safely.
+
+  Don't forget to free any malloc'ed memory!
+ ****/
+void hash_table_remove(HashTable *ht, char *key)
+{
+
+}
+
+/****
+  Fill this in.
+
+  Should search the entire list of LinkedPairs for existing
+  keys.
+
+  Return NULL if the key is not found.
+ ****/
+char *hash_table_retrieve(HashTable *ht, char *key)
+{
+  return NULL;
+}
+
+/****
+  Fill this in.
+
+  Don't forget to free any malloc'ed memory!
+ ****/
+void destroy_hash_table(HashTable *ht)
+{
+
+}
+
+/****
+  Fill this in.
+
+  Should create a new hash table with double the capacity
+  of the original and copy all elements into the new hash table.
+
+  Don't forget to free any malloc'ed memory!
+ ****/
+HashTable *hash_table_resize(HashTable *ht)
+{
+  HashTable *new_ht;
+
+  return new_ht;
+}
+
+
+#ifndef TESTING
+int main(void)
+{
+  struct HashTable *ht = create_hash_table(2);
+
+  hash_table_insert(ht, "line_1", "Tiny hash table\n");
+  hash_table_insert(ht, "line_2", "Filled beyond capacity\n");
+  hash_table_insert(ht, "line_3", "Linked list saves the day!\n");
+
+  printf("%s", hash_table_retrieve(ht, "line_1"));
+  printf("%s", hash_table_retrieve(ht, "line_2"));
+  printf("%s", hash_table_retrieve(ht, "line_3"));
+
+  int old_capacity = ht->capacity;
+  ht = hash_table_resize(ht);
+  int new_capacity = ht->capacity;
+
+  printf("\nResizing hash table from %d to %d.\n", old_capacity, new_capacity);
+
+  destroy_hash_table(ht);
+
+  return 0;
+}
+#endif
