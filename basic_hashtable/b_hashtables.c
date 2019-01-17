40b611ca240ffef9004d3661b4aa9b5805d313f4
Brian Ruff <thelonewolf198927@gmail.com>
2019-01-17 15:39:45 -0500

    destroy table

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -103,7 +103,9 @@ void hash_table_insert(BasicHashTable *ht, char *key, char *value)
  ****/
 void hash_table_remove(BasicHashTable *ht, char *key)
 {
-
+    int hashed_key = hash(key, ht->capacity);
+    destroy_pair(ht->storage[hashed_key]);
+    ht->storage[hashed_key] = NULL;
 }
 
 /****
