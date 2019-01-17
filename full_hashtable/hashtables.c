536da71269221db1b58bb0c76eee45640b447bd0
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2019-01-16 20:15:02 -0500

    Cleaned up

--- full_hashtable/hashtables.c
+++ full_hashtable/hashtables.c
@@ -91,13 +91,7 @@ HashTable *create_hash_table(int capacity)
  ****/
 void hash_table_insert(HashTable *ht, char *key, char *value)
 {
-  int hashIndex = hash(key, ht->capacity);
-  if (ht->storage[hashIndex] != NULL) {
-    printf("Existing key, value has been overwritten");
-    destroy_pair(ht->storage[hashIndex]);
-  }
-  ht->storage[hashIndex] = create_pair(key, value);
-
+  
 }
 
 /****
@@ -110,13 +104,7 @@ void hash_table_insert(HashTable *ht, char *key, char *value)
  ****/
 void hash_table_remove(HashTable *ht, char *key)
 {
-  int hashIndex = hash(key, ht->capacity);
-  if (ht->storage[hashIndex] != NULL) {
-    destroy_pair(ht->storage[hashIndex]);
-    ht->storage[hashIndex] = NULL;
-  } else {
-    printf("There is no value to remove");
-  }
+ 
 
 }
 
