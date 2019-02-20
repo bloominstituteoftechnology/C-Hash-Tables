1ddfbaa0a02cc0eab17b92fcbe025b15e35de825
Patrick Thompson <patrick.jthompson93@gmail.com>
2019-02-20 12:46:01 -0800

    finishes up to retrieve

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -72,10 +72,15 @@ unsigned int hash(char *str, int max)
  ****/
 BasicHashTable *create_hash_table(int capacity)
 {
+  // Create a new hash table named ht
   BasicHashTable *ht;
+  // Malloc the size of hash table
   ht = malloc(sizeof(BasicHashTable));
+  // set capacity to passed in capacity
   ht->capacity = capacity;
+  // use calloc to set storage bites and set all to 0
   ht->storage = calloc(capacity, sizeof(Pair));
+  // return the new Hash table
   return ht;
 }
 
@@ -88,8 +93,11 @@ BasicHashTable *create_hash_table(int capacity)
  ****/
 void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
+  // Create a pair by sending the key and value
   Pair *pair = create_pair(key, value);
+  // create a key by hashing
   unsigned int hashed_key = hash(key, ht->capacity);
+  // set the storage key and value
   ht->storage[hashed_key] = pair;
 }
 
@@ -100,15 +108,29 @@ void hash_table_insert(BasicHashTable *ht, char *key, char *value)
  ****/
 void hash_table_remove(BasicHashTable *ht, char *key)
 {
+  // create a key by hashing
+  unsigned int hashed_key = hash(key, ht->capacity);
+  // free up the alloc memory
+  free(ht->storage[hashed_key]);
+  // set the value to null
+  ht->storage[hashed_key] = NULL;
 }
 
 /****
   Fill this in.
-
   Should return NULL if the key is not found.
  ****/
 char *hash_table_retrieve(BasicHashTable *ht, char *key)
 {
+  // create a key by hasing
+  unsigned int hashed_key = hash(key, ht->capacity);
+  // Map that pair into a result
+  Pair *result = ht->storage[hashed_key];
+  // if result, return the value
+  if (result)
+  {
+    return result->value;
+  }
   return NULL;
 }
 
