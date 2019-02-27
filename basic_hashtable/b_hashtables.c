844586bf9b1081d338d3f2e0b626ea5984e501f4
Kat Fries <katherynejf@gmail.com>
2019-02-27 18:43:01 -0500

    all tests passing

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -99,15 +99,20 @@ unsigned int hash(char *str, int max)
     - size − This is the size of elements.
   a = (int*)calloc(n, sizeof(int));
   Difference between malloc and calloc:
-   malloc does not set the memory to zero 
-   calloc sets allocated memory to zero.
+   malloc 
+    does not set the memory to zero 
+    need for loop to set each spot to NULL
+    1 parameter size of
+   calloc 
+    sets all slot of allocated memory to zero.
+    2 parameters: number of slots & size
 
  ****/
 BasicHashTable *create_hash_table(int capacity)
 {
-  BasicHashTable *ht = calloc(capacity, sizeof(BasicHashTable));
+  BasicHashTable *ht = malloc(sizeof(BasicHashTable));
   ht->capacity = capacity;
-  ht->storage = (Pair**)calloc(capacity, sizeof(Pair));
+  ht->storage = calloc(capacity, sizeof(Pair*));
 
   // for loop using malloc: 
     // len = capacity 
@@ -126,11 +131,15 @@ BasicHashTable *create_hash_table(int capacity)
  ****/
 void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
-  // for(int i = 0; i < ht->capacity; i++) {
-  //   if (key == ht->storage->key) {
-  //     fprintf(stderr, "Warning: %d has already been used \n", key);
-  //   }
-  // }
+  // unsigned int hash(char *str, int max);
+  unsigned int hash_key = hash(key, ht->capacity);
+
+  // obj
+  if (ht->storage[hash_key]) {
+      fprintf(stderr, "Warning: overwriting %s with a different key \n", key);
+      destroy_pair(ht->storage[hash_key]);
+  }
+  ht->storage[hash_key] = create_pair(key, value);
 }
 
 /****
@@ -140,7 +149,9 @@ void hash_table_insert(BasicHashTable *ht, char *key, char *value)
  ****/
 void hash_table_remove(BasicHashTable *ht, char *key)
 {
-
+  unsigned int hash_key = hash(key, ht->capacity);
+  destroy_pair(ht->storage[hash_key]);
+  ht->storage[hash_key] = NULL;
 }
 
 /****
@@ -150,7 +161,13 @@ void hash_table_remove(BasicHashTable *ht, char *key)
  ****/
 char *hash_table_retrieve(BasicHashTable *ht, char *key)
 {
-  return NULL;
+  // unsigned int hash(char *str, int max);
+  unsigned int hash_key = hash(key, ht->capacity);
+
+  if (!(ht->storage[hash_key])) {
+    return NULL;
+  }
+  return ht->storage[hash_key]->value;
 }
 
 /****
@@ -160,14 +177,29 @@ char *hash_table_retrieve(BasicHashTable *ht, char *key)
  ****/
 void destroy_hash_table(BasicHashTable *ht)
 {
-  //   for(int i = 0; i < ht->capacity; i++) {
-  //     destroy_pair(ht->storage[i]);
-  //     free(ht->storage[i]);
-  //   }
-  //   if (ht != NULL) {
-  //     free(ht->storage);
-  //     free(ht);
-  // }
+    for(int i = 0; i < ht->capacity; i++) {
+      destroy_pair(ht->storage[i]);
+      free(ht->storage[i]);
+    }
+    if (ht != NULL) {
+      free(ht->storage);
+      free(ht);
+  }
+}
+
+/*****
+ * Utility function to print an array.
+ *****/
+void ht_print(BasicHashTable *ht) {
+  printf("[");
+  for (int i = 0 ; i < ht->capacity ; i++) {
+    printf("%p", ht->storage[i]);
+    if (i != ht->capacity - 1) {
+      printf(",");
+    }
+  }
+  printf(", capacity: %d", ht->capacity);
+  printf("]\n");
 }
 
 
@@ -175,6 +207,7 @@ void destroy_hash_table(BasicHashTable *ht)
 int main(void)
 {
   struct BasicHashTable *ht = create_hash_table(16);
+  ht_print(ht);
 
   hash_table_insert(ht, "line", "Here today...\n");
 
@@ -183,7 +216,7 @@ int main(void)
   hash_table_remove(ht, "line");
 
   if (hash_table_retrieve(ht, "line") == NULL) {
-    printf("...gone tomorrow. (success)\n");
+    printf("\n...gone tomorrow. (success)\n");
   } else {
     fprintf(stderr, "ERROR: STILL HERE\n");
   }
