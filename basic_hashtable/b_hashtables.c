5dab3cceb3a760526888bdc4c02ec0518089e3b8
Kat Fries <katherynejf@gmail.com>
2019-02-27 19:32:42 -0500

    added a not null check to destroy

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -178,8 +178,10 @@ char *hash_table_retrieve(BasicHashTable *ht, char *key)
 void destroy_hash_table(BasicHashTable *ht)
 {
     for(int i = 0; i < ht->capacity; i++) {
-      destroy_pair(ht->storage[i]);
-      free(ht->storage[i]);
+      if(ht->storage[i]) {
+        destroy_pair(ht->storage[i]);
+        free(ht->storage[i]);
+      }
     }
     if (ht != NULL) {
       free(ht->storage);
